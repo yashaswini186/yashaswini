4ec8ba4176c3f24d92dfb684ad88ecee395b6072
yashaswini186 <yashushivalingaiah2004@gmail.com>
2024-10-16 01:53:29 -0500

    Add files via upload

---
+++ lab4b.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[1,2,3,4,5]
+plt.scatter(x,y,color='blue')
+plt.xlabel("xaxis",fontsize=12)
+plt.ylabel("yaxis",fontsize=12)
+plt.title("yashaswini s-1KI23CS186")
+plt.show()
