4349562d814f8f7fdc6c6f664e346d3585177acf
yashaswini186 <yashushivalingaiah2004@gmail.com>
2024-10-16 01:52:56 -0500

    Add files via upload

---
+++ lab4a.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+std=['sslc','puc','1sem','2sem']
+values=[500,600,620,680]
+plt.bar(std,values,color='blue')
+plt.xlabel("education",fontsize=12)
+plt.ylabel("marks obtained",fontsize=12)
+plt.title("yashaswini s-1kI23CS186")
+plt.show()
