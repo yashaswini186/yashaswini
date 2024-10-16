a8066f4138c9837635e8c4dd8016f27cba2d4a6d
yashaswini186 <yashushivalingaiah2004@gmail.com>
2024-10-16 02:09:38 -0500

    Add files via upload

---
+++ 5b.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+CARS=['audi','benz','rangerover','innova','swift','alto']
+data=[23,45,36,78,97,12]
+plt.figure(figsize=(10,7))
+plt.pie(data,labels=CARS)
+plt.title("yashaswini s-1KI23CS186")
+plt.show()
