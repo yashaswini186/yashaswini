48079e4b465a11dda669c034126b5467f3ac7d96
yashaswini186 <yashushivalingaiah2004@gmail.com>
2024-10-16 02:01:13 -0500

    Add files via upload

---
+++ 5a.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.hist([5,9,10,13,16,18,22,25,28,31,35,39],bins=4)
+plt.xlabel("Distribution of Age",fontsize=16)
+plt.ylabel("Frequency",fontsize=16)
+plt.title("yashaswini s-1KI23CS186")
+plt.show()
